30f9ee20689a3d24121c5d7090f59d96bd1e6ca1
jettairlines <lwanghe2@gmail.com>
2016-01-29 17:19:21 -0600

    better translated x86 -> human language

--- sp1-2.c
+++ sp1-2.c
@@ -1,7 +1,11 @@
-/* HI TA
- * WTF is the star
+/* HI GROUP
+ * make sure all this is translated correctly before trying to make sense of it
+ * 1. x86 -> human language		; approved by Jett & Branden
+ * 2. *pending* human language -> logically simplified human language
  */
 
+//	n + mem[r1] + mem[r2]*c
+//	32bit arch
 
 struct op_list {
 	struct op_list *next;
@@ -17,27 +21,25 @@ int eval(struct op_list *, int){
 	//b = 8+m[b]
 	//a = m[ 12+m[b] ]		//something is 4 bits/bytes?
 
-	//pool
+	//POOL
 		//b = m[ m[b] ]
 
 		//if 0
 			//pop to b
 		//else
-			//loop
+			//LOOP
 				// d = m[ 4+m[b] ]
 				// if d > 2 (unsigned)
 					//Td
-					// b *= 8
-					// LOOP FROM POOL
-				// else *** TODO: WTF DO WE DO HERE
-					// go to mem[table + d*4] (probably reading 0,1,2 from table array)
-
-		//i think we just leave
-		//n return
+						// a *= m[ 8+m[b] ]
+						// LOOP FROM POOL
+				// else
+					// go to table[d] (simplified all bit mults)
+		//return
 
 
 
-		//table: arr of functions?
+		//table: is array of functions
 		//T0
 			// a -= m[ 8+m[b] ]
 			// LOOP FROM POOL
@@ -45,15 +47,14 @@ int eval(struct op_list *, int){
 			//c = m[ 8+m[b] ]
 			//c = c & x1f
 			//d = 1
-			//d = d >> m[ cl ] ***TODO : OK WTF IS CL
+			//d = d >> m[ last 8 bits(1/4) of c ]
 			//d = !d
 			//a = a & d
 			//LOOP FROM POOL
 		//T2
 			//push a
-			// TODO: SOMETHING ABOUT STAR
-			// sp += 4
-			// LOOP FROM POOLt
+			//call func at m[ m[b]+12 ] 
+			//sp += 4
+			//LOOP FROM POOL
 
-	n + mem[r1] + mem[r2]*c
 }
